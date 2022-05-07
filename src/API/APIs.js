49a571a83a9aa007bea83b7490ccaa83884e5171
jchen1379 <jianhuichen1379@gmail.com>
2022-05-07 17:19:18 -0400

    added loggin for api env var

--- src/API/APIs.js
+++ src/API/APIs.js
@@ -1,5 +1,9 @@
 const baseURL = process.env.REACT_APP_WEBSITE_TRAFFIC_MONITOR_API;
 
+if(!baseURL){
+  console.log("API URL is not found in ENV, please set REACT_APP_WEBSITE_TRAFFIC_MONITOR_API=$ApiUrl.");
+}
+
 export const domainsVisitsDataByDays = (days) => {
   return `${baseURL}/get_traffic_data/visits/days/${days}`;
 }
